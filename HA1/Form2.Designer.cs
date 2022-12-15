c0e894f82cf700ee4746ef955f6fd7f2744fce97
Asya T <anastasya.talakutskaya@yandex.com>
2022-12-15 22:37:53 +0300

    Добавьте файлы проекта.

---
+++ HA1/Form2.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace HA1
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.messages = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // messages
+            // 
+            this.messages.AutoSize = true;
+            this.messages.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.messages.Location = new System.Drawing.Point(49, 33);
+            this.messages.Name = "messages";
+            this.messages.Size = new System.Drawing.Size(0, 45);
+            this.messages.TabIndex = 0;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.messages);
+            this.Name = "Form2";
+            this.Text = "Сообщения";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label messages;
+    }
+}
---
+++ HA1/Form2.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Windows.Forms;
+
+namespace HA1
+{
+    public partial class Form2 : Form
+    {
+        class User
+        {
+            public string name;//имя и фамилия пользователя
+        }
+
+        class Chat
+        {
+            public string sender;//отправитель сообщения
+            public string message;//текст сообщения
+            public override string ToString()
+            {
+                return sender + "\n" + message + "\n\n";
+            }
+        }
+
+        public Form2(string fi)
+        {
+            InitializeComponent();
+
+            User user = new User() { name = fi };
+            WebClient client = new WebClient();
+            string url = "http://194.87.99.14/auth";
+            string enterstr = JsonConvert.SerializeObject(user);
+            string jwt = client.UploadString(url, enterstr);
+            //Задание 1
+            url = "http://194.87.99.14/messages";
+            string response = client.UploadString(url, jwt);
+            List<Chat> chat = JsonConvert.DeserializeObject<List<Chat>>(response);
+            foreach (Chat el in chat)
+                messages.Text += el;
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
